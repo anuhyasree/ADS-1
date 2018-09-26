7cf3f486f18fd891d9708357ca82014113d1be82
anuhya45 <sreeanuhya45@msitprogram.net>
2018-09-26 23:12:02 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

---
+++ Assignmnet/M3/3-sum/Solution.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+import java.util.Arrays;
+class Solution {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        threeSum t = new threeSum();
+        int n = s.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = s.nextInt();
+        }
+        t.findTrip(arr);
+    }
+}
+class threeSum {
+	int count;
+	threeSum() {
+		count = 0;
+	}
+	void findTrip(int[]arr) {
+		int len = arr.length;
+		Arrays.sort(arr);
+		for (int i = 0; i < len - 1; i++) {
+			int j = i + 1;
+			int r = len - 1;
+			int x = arr[i];
+			while (j < r) {
+				int sum = x + arr[j]+arr[r];
+				if (sum == 0) {
+					count++;
+					j++;
+					r--;
+				} else if (sum < 0) {
+					j++;
+
+				}else {
+					r--;
+				}
+			}
+
+		}
+		System.out.println(count);
+	}
+}
