120a565fabdc4735466a3f9af6b1a7aed2386671
anuhya45 <sreeanuhya45@msitprogram.net>
2018-09-26 23:41:45 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- Assignmnet/M3/3-sum/Solution.java
+++ Assignmnet/M3/3-sum/Solution.java
@@ -5,71 +5,80 @@ import java.util.Arrays;
  * Class for solution.
  */
 class Solution {
-	/**.
-	 * { function_description }
-	 *
-	 * @param      args  The arguments
-	 */
-	/**.
-	 * Constructs the object.
-	 */
-	Solution() {
-		/**.
-		 * { item_description }
-		 */
-	}
-	/**.
-	 * { function_description }
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(String[] args) {
-		/**.
-		 * { var_description }
-		 */
-		Scanner s = new Scanner(System.in);
-		threeSum t = new threeSum();
-		int n = s.nextInt();
-		int[] arr = new int[n];
-		for (int i = 0; i < n; i++) {
-			arr[i] = s.nextInt();
-		}
-		t.findTrip(arr);
-	}
+    /**.
+     * { function_description }
+     *
+     * @param      args  The arguments
+     */
+    /**.
+     * Constructs the object.
+     */
+    Solution() {
+        /**.
+         * { item_description }
+         */
+    }
+    /**.
+     * { function_description }
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        /**.
+         * { var_description }
+         */
+        Scanner s = new Scanner(System.in);
+        ThreeSum t = new ThreeSum();
+        int n = s.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = s.nextInt();
+        }
+        t.findTrip(arr);
+    }
 }
 /**.
  * Class for three sum.
  */
-class threeSum {
-	/**.
-	 * { var_description }
-	 */
-	int count;
-	threeSum() {
-		count = 0;
-	}
-	void findTrip(int[]arr) {
-		int len = arr.length;
-		Arrays.sort(arr);
-		for (int i = 0; i < len - 1; i++) {
-			int j = i + 1;
-			int r = len - 1;
-			int x = arr[i];
-			while (j < r) {
-				int sum = x + arr[j] + arr[r];
-				if (sum == 0) {
-					count++;
-					j++;
-					r--;
-				} else if (sum < 0) {
-					j++;
+class ThreeSum {
+    /**.
+     * { var_description }
+     */
+    private int count;
+    /**.
+     * Constructs the object.
+     */
+    ThreeSum() {
+        count = 0;
+    }
+    /**.
+     * { function_description }
+     *
+     * @param      arr   The arr
+     */
+    void findTrip(final int[]arr) {
+        int len = arr.length;
+        Arrays.sort(arr);
+        for (int i = 0; i < len - 1; i++) {
+            int j = i + 1;
+            int r = len - 1;
+            int x = arr[i];
+            while (j < r) {
+                int sum = x + arr[j] + arr[r];
+                if (sum == 0) {
+                    count++;
+                    j++;
+                    r--;
+                } else if (sum < 0) {
+                    j++;
 
-				} else {
-					r--;
-				}
-			}
+                } else {
+                    r--;
+                }
+            }
 
-		}
-		System.out.println(count);
-	}
+        }
+        System.out.println(count);
+    }
 }
+
