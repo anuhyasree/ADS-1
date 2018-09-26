3b4266b8241e4f02e7e425e0c768fa528fa89966
anuhya45 <sreeanuhya45@msitprogram.net>
2018-09-26 23:37:34 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- Assignmnet/M3/3-sum/Solution.java
+++ Assignmnet/M3/3-sum/Solution.java
@@ -1,18 +1,49 @@
 import java.util.Scanner;
 import java.util.Arrays;
+
+/**.
+ * Class for solution.
+ */
 class Solution {
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        threeSum t = new threeSum();
-        int n = s.nextInt();
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = s.nextInt();
-        }
-        t.findTrip(arr);
-    }
+	/**.
+	 * { function_description }
+	 *
+	 * @param      args  The arguments
+	 */
+	/**.
+	 * Constructs the object.
+	 */
+	Solution() {
+		/**.
+		 * { item_description }
+		 */
+	}
+	/**.
+	 * { function_description }
+	 *
+	 * @param      args  The arguments
+	 */
+	public static void main(String[] args) {
+		/**.
+		 * { var_description }
+		 */
+		Scanner s = new Scanner(System.in);
+		threeSum t = new threeSum();
+		int n = s.nextInt();
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = s.nextInt();
+		}
+		t.findTrip(arr);
+	}
 }
+/**.
+ * Class for three sum.
+ */
 class threeSum {
+	/**.
+	 * { var_description }
+	 */
 	int count;
 	threeSum() {
 		count = 0;
@@ -25,7 +56,7 @@ class threeSum {
 			int r = len - 1;
 			int x = arr[i];
 			while (j < r) {
-				int sum = x + arr[j]+arr[r];
+				int sum = x + arr[j] + arr[r];
 				if (sum == 0) {
 					count++;
 					j++;
@@ -33,7 +64,7 @@ class threeSum {
 				} else if (sum < 0) {
 					j++;
 
-				}else {
+				} else {
 					r--;
 				}
 			}
