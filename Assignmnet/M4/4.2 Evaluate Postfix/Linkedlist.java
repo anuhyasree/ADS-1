bb76567baaecd8228785e03c38b785edc00d5dbd
anuhya45 <sreeanuhya45@msitprogram.net>
2018-09-28 16:23:03 +0530

    "check_style_score -> 4 of 4 passed. style score: 1/1 "

--- Assignmnet/M4/4.2 Evaluate Postfix/Linkedlist.java
+++ Assignmnet/M4/4.2 Evaluate Postfix/Linkedlist.java
@@ -5,7 +5,7 @@ class Node {
     /**
      * Data in string.
      */
-    String data;
+    int data;
     /**
      * Next node.
      */
@@ -19,91 +19,34 @@ class Linkedlist {
     /**
      * An empty node.
      */
-    private Node start = null;
+    private Node start = new Node();
     /**
      * Pushing an element into list.
      *
-     * @param      ch    { given String }
+     * @param      value  The value
      */
-    void push(final String ch) {
+    void push(final int value) {
         Node val = start;
         start = new Node();
-        start.data = ch;
+        start.data = value;
         start.next = val;
     }
     /**
      * Popping element from list.
      *
-     * @return     { String value }
+     * @return     { int value }
      */
-    String pop() {
-        if (start == null) {
-            return "e";
-        }
-        String r = start.data;
+    int pop() {
+        int c = start.data;
         start = start.next;
-        return r;
+        return c;
     }
     /**
      * First value.
      *
      * @return     { value }
      */
-    public Node gethead() {
-        return start;
-    }
-}
-/**
- * Class for balanced.
- */
-class Balanced {
-    /**
-     * Constructs the object.
-     */
-    Balanced() {
-
-    }
-    /**
-     * Checking if the pair is matched or not.
-     *
-     * @param      str1  The string 1
-     * @param      str2  The string 2
-     *
-     * @return     { a boolean }
-     */
-    boolean Matchingpair (String str1, String str2) {
-        if (str1.equals("(") && str2.equals(")")) {
-            return true;
-        }
-        if (str1.equals("[") && str2.equals("]")) {
-            return true;
-        }
-        if (str1.equals("{") && str2.equals("}")) {
-            return true;
-        }
-        return false;
-    }
-    /**
-     * Balancing of parnathesis.
-     *
-     * @param      array  The array
-     *
-     * @return     { boolean based on matching }
-     */
-    boolean balancing(final String[] array) {
-        Linkedlist ll = new Linkedlist();
-        for (int i = 0; i < array.length; i++) {
-            if (array[i].equals("(") || array[i].equals("[") || array[i].equals("{")) {
-                ll.push(array[i]);
-            }
-            if (array[i].equals(")") || array[i].equals("]") || array[i].equals("}")) {
-                if (Matchingpair(ll.pop(), array[i])) {
-                    continue;
-                } else {
-                    return false;
-                }
-            }
-        }
-        return ll.gethead() == null;
+    public boolean isEmpty() {
+        return (start.next == null);
     }
 }
--- Assignmnet/M4/4.2 Evaluate Postfix/Solution.java
+++ Assignmnet/M4/4.2 Evaluate Postfix/Solution.java
@@ -1,11 +1,15 @@
-import java.util.Scanner;
-//import java.util.Arrays;
-
-
 /**
- * Class for using ll.
+ * { Importing Scannner }.
  */
-final class Solution {
+import java.util.Scanner;
+/**
+ * { Importing Arrays }.
+ */
+import java.util.Arrays;
+/**
+ * Class for solution.
+ */
+public final class Solution {
     /**
      * Constructs the object.
      */
@@ -13,24 +17,34 @@ final class Solution {
         //Empty constructor.
     }
     /**
-     * Main method.
+     * Main function.
      *
      * @param      args  The arguments
      */
     public static void main(final String[] args) {
+        Linkedlist ll = new Linkedlist();
+        int popelement1 = 0;
+        int popelement2 = 0;
         Scanner sc = new Scanner(System.in);
-        int n = Integer.parseInt(sc.nextLine());
-        while (n != 0) {
-            String[] input = sc.nextLine().split("");
-            Balanced b = new Balanced();
-            if (b.balancing(input)) {
-                System.out.println("YES");
+        String[] input = sc.nextLine().split(" ");
+        for (int i = 0; i < input.length; i++) {
+           if (!(Arrays.asList("+", "-", "*", "/").contains(input[i]))) {
+                ll.push(Integer.parseInt(input[i]));
             } else {
-                System.out.println("NO");
+                popelement1 = ll.pop();
+                popelement2 = ll.pop();
+                if (input[i].equals("+")) {
+                    ll.push(popelement1 + popelement2);
+                } else if (input[i].equals("-")) {
+                    ll.push(popelement1 - popelement2);
+                } else if (input[i].equals("*")) {
+                    ll.push(popelement1 * popelement2);
+                } else {
+                    ll.push(popelement1 / popelement2);
+                }
             }
-            n--;
         }
+        System.out.println(ll.pop());
     }
 }
-
 
