2abb1ab0633e7af0aa37ca61bb0eb323f9d6fb3c
anuhya45 <sreeanuhya45@msitprogram.net>
2018-09-30 19:51:16 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- Assignmnet/M5/5.2 Josephus Problem/Solution.java
+++ Assignmnet/M5/5.2 Josephus Problem/Solution.java
@@ -2,10 +2,10 @@ import java.util.Scanner;
 /**.
 Node the class
 */
-class node {
+class Node {
     int data;
-    node next;
-    node(int d) {
+    Node next;
+    Node(int d) {
         data = d;
     }
 }
@@ -15,26 +15,29 @@ Likedlist the solution
 class linkedlist {
     int size;
     int count;
-    node head,tail;
-    String result ="";
+    Node head, tail;
+    String result = "";
     linkedlist(int s, int c) {
         size = s;
         count = c;
     }
 
+    /**.
+     * { function_description }
+     */
     public void josephproblem() {
-        node newnode = new node(0);
-        head = newnode;
-        tail = newnode;
-        for(int i = 1; i < size; i++) {
-            node n = new node(i);
+        Node newNode = new Node(0);
+        head = newNode;
+        tail = newNode;
+        for (int i = 1; i < size; i++) {
+            Node n = new Node(i);
             tail.next = n;
             tail = n;
         }
         int c;
-        node temp;
-        while(size != 1) {
-            for(c = 1; c < count; c++) {
+        Node temp;
+        while (size != 1) {
+            for (c = 1; c < count; c++) {
                 temp = head;
                 head = head.next;
                 tail.next = temp;
@@ -53,13 +56,13 @@ class linkedlist {
 Solution the class
 @param args the arguments
 */
-final class Solution{
+final class Solution {
     public static void main(final String[] args) {
         linkedlist l;
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         sc.nextLine();
-        for(int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             String[] tokens = sc.nextLine().split(" ");
             l = new linkedlist(Integer.parseInt(tokens[0]), Integer.parseInt(tokens[1]));
             l.josephproblem();
