4b8a94ab4dd8b69453ef441dfcb36cd5ae9f931a
anuhya45 <sreeanuhya45@msitprogram.net>
2018-09-30 20:22:05 +0530

    "check_style_score -> 5 of 5 passed. style score: 1/1 "

--- Assignmnet/M5/5.2 Josephus Problem/Solution.java
+++ Assignmnet/M5/5.2 Josephus Problem/Solution.java
@@ -1,72 +1,30 @@
 import java.util.Scanner;
-/**.
-Node the class
-*/
-class Node {
-    int data;
-    Node next;
-    Node(int d) {
-        data = d;
-    }
-}
-/**.
-Likedlist the solution
-*/
-class linkedlist {
-    int size;
-    int count;
-    Node head, tail;
-    String result = "";
-    linkedlist(int s, int c) {
-        size = s;
-        count = c;
-    }
 
-    /**.
-     * { function_description }
+/**
+ * Class to handle inputs and deliver outputs.
+ */
+public final class Solution {
+    /**
+     * Constructs the object.
      */
-    public void josephproblem() {
-        Node newNode = new Node(0);
-        head = newNode;
-        tail = newNode;
-        for (int i = 1; i < size; i++) {
-            Node n = new Node(i);
-            tail.next = n;
-            tail = n;
-        }
-        int c;
-        Node temp;
-        while (size != 1) {
-            for (c = 1; c < count; c++) {
-                temp = head;
-                head = head.next;
-                tail.next = temp;
-                tail = temp;
-            }
-            result += head.data + " ";
-            head = head.next;
-            c = 1;
-            size--;
-        }
-        result += head.data;
-        System.out.println(result);
+    private Solution() {
+
     }
-}
-/**.
-Solution the class
-@param args the arguments
-*/
-final class Solution {
+    /**
+     * The main function to take inputs and deliver outputs.
+     *
+     * @param      args  The arguments
+     */
     public static void main(final String[] args) {
-        linkedlist l;
+        Problem l;
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         sc.nextLine();
         for (int i = 0; i < n; i++) {
             String[] tokens = sc.nextLine().split(" ");
-            l = new linkedlist(Integer.parseInt(tokens[0]), Integer.parseInt(tokens[1]));
+            l = new Problem(Integer.parseInt(tokens[0]),
+                Integer.parseInt(tokens[1]));
             l.josephproblem();
         }
     }
-
 }
