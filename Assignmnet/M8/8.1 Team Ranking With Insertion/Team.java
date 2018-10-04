e7431b148b9218a69c76841936b894eab065a499
anuhya45 <sreeanuhya45@msitprogram.net>
2018-10-04 22:27:01 +0530

    "ads1_m8_a1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignmnet/M8/8.1 Team Ranking With Insertion/Team.java
+++ Assignmnet/M8/8.1 Team Ranking With Insertion/Team.java
@@ -8,6 +8,7 @@ class Team {
 	public void addCricketTeam(CricketTeam t) {
 		teams[size++] = t;
 	}
+	// The complexity of this recursion is O(n)
 	public String toString() {
 		String s = "";
 		for (int i = 0; i < size; i++) {
@@ -16,27 +17,23 @@ class Team {
 		return s.substring(0, s.length() - 1);
 	}
 	public void exch(CricketTeam[] teams, int i, int j) {
-		//System.out.println(";;;;;;");
 		CricketTeam ct = teams[j];
 		teams[j] = teams[i];
-		teams[i] = ct; 
+		teams[i] = ct;
 	}
+		// The complexity of this recursion is O(n^2)
 	public void selectionSort() {
-		//System.out.println("-------");
 		for (int i = 0; i < size - 1; i++) {
 			int min = i;
 			for (int j = i + 1; j < size; j++) {
-				//System.out.println("[[[[[[");
 				if (less(teams, j, min)) {
 					min = j;
-				} 
+				}
 			}
 			exch(teams, i, min);
-			//System.out.println(teams[i]);
         }
 	}
 	boolean less(CricketTeam[] arr, int i, int j) {
-		//System.out.println("0000000");
 		return  arr[i].compareTo(arr[j]) > 0;
 	}
 }
