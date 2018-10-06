a8c0e2a9d60c818ac03bc8f5c72e74d611c297d8
anuhya45 <sreeanuhya45@msitprogram.net>
2018-10-06 13:04:43 +0530

    "ads1_m12_a1: 2/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignmnet/M12/Assignment-1/Student.java
+++ Assignmnet/M12/Assignment-1/Student.java
@@ -69,18 +69,42 @@ class Student implements Comparable<Student> {
      * @return     { description_of_the_return_value }
      */
     public int compareTo (final Student that) {
-        if (this.total > that.total) { return 1; }
-        if (this.total < that.total) { return -1; }
-        if (this.sub3 > that.sub3) { return 1; }
-        if (this.sub3 < that.sub3) { return -1; }
-        if (this.sub2 > that.sub2) { return 1; }
-        if (this.sub2 < that.sub2) { return -1; }
-        if (this.year > that.year) { return 1; }
-        if (this.year < that.year) { return -1; }
-        if (this.month > that.month) { return 1; }
-        if (this.month < that.month) { return -1; }
-        if (this.day > that.day) { return 1; }
-        if (this.day < that.day) { return -1; }
+        if (this.total > that.total) {
+            return 1;
+        }
+        if (this.total < that.total) {
+            return -1;
+        }
+        if (this.sub3 > that.sub3) {
+            return 1;
+        }
+        if (this.sub3 < that.sub3) {
+            return -1;
+        }
+        if (this.sub2 > that.sub2) {
+            return 1;
+        }
+        if (this.sub2 < that.sub2) {
+            return -1;
+        }
+        if (this.year > that.year) {
+            return 1;
+        }
+        if (this.year < that.year) {
+            return -1;
+        }
+        if (this.month > that.month) {
+            return 1;
+        }
+        if (this.month < that.month) {
+            return -1;
+        }
+        if (this.day > that.day) {
+            return 1;
+        }
+        if (this.day < that.day) {
+            return -1;
+        }
         return 0;
     }
 
